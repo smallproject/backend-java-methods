08d3aabbe27bf54d8fced99a8151aa1a4bed63db
smallproject <j.a.villas@live.com.ph>
2024-06-03 03:44:44 +0200

    opdracht 2

--- src/main/java/B_Dialoog.java
+++ src/main/java/B_Dialoog.java
@@ -6,17 +6,25 @@ public class B_Dialoog {
         // De methode heeft als parameter de tekst die gesproken moet worden.
         // De volgende zin wordt steeds door de ander gesproken.
         // Vervang System.out.println() steeds door je eigen method.
-        System.out.println("Hey"); //Bert
-        System.out.println("Hey"); //Ernie
-        System.out.println("Welcome to this wonderful conversation");
-        System.out.println("Thnx, I'm looking forward to it");
-        System.out.println("How about this weather?");
-        System.out.println("I really don't mind the cold....");
-        System.out.println("And did you see that random program on that random channel?");
-        System.out.println("Well... I did watch a random program on a random channel...");
-        System.out.println("Thank you for a wonderful conversation!");
-        System.out.println("Cya");
+        bertSays("Hey"); //Bert
+        ernieSays("Hey"); //Ernie
+        bertSays("Welcome to this wonderful conversation");
+        ernieSays("Thnx, I'm looking forward to it");
+        bertSays("How about this weather?");
+        ernieSays("I really don't mind the cold....");
+        bertSays("And did you see that random program on that random channel?");
+        ernieSays("Well... I did watch a random program on a random channel...");
+        bertSays("Thank you for a wonderful conversation!");
+        ernieSays("Cya");
     }
 
     // nieuwe methods komen hier
+
+    private static void bertSays(String text){
+        System.out.println("Bert says: " + text);
+    }
+
+    private static void ernieSays(String text){
+        System.out.println(text + " says Ernie");
+    }
 }
